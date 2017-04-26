ca2847b09922e01e52a11d5b30e090a5d7e1ebb8
ShayanJa <shay.talebi@gmail.com>
2017-04-26 14:41:35 -0700

    added solution test

--- tests/solutions_test.go
+++ tests/solutions_test.go
@@ -6,7 +6,11 @@ import (
 	"work/xprincipia/backend/gorm"
 )
 
-//test Solutions api
+/*
+Test Solutions API
+*/
+
+//Test GetSolutionByID function in Gorm
 func TestGetSolutionByID(t *testing.T) {
 
 	db := SetupTestingDB()
@@ -24,4 +28,25 @@ func TestGetSolutionByID(t *testing.T) {
 	assert.Equal(t, actualResult.ProblemID, expectedResult.ProblemID, "the problem ID's should be the same")
 	assert.Equal(t, actualResult.OriginalPosterUsername, expectedResult.OriginalPosterUsername, "the OriginalPosterUsername's should be the same")
 	assert.Equal(t, actualResult.Title, expectedResult.Title, "the Title's should be the same")
+}
+
+func TestGetSolutionsByProblemID(t *testing.T) {
+
+	db := SetupTestingDB()
+	defer db.Close()
+
+	actualResult := gorm.GetSolutionsByProblemID(1)
+	expectedResult := []gorm.Solution{}
+	s := gorm.Solution{}
+	s.GetSolutionByID(1)
+	expectedResult = append(expectedResult, s)
+	s = gorm.Solution{}
+	s.GetSolutionByID(2)
+	expectedResult = append(expectedResult, s)
+	s = gorm.Solution{}
+	s.GetSolutionByID(3)
+	expectedResult = append(expectedResult, s)
+
+	assert.Equal(t, actualResult, expectedResult, "The should be 3 solutions with the same values")
+
 }
